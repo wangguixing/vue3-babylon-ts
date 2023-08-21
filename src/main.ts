253cbb8921abcfe25e525f6fa7874764aaf54b41
GranceWang <70499705+wangguixing@users.noreply.github.com>
2023-08-21 09:57:53 +0800

    fix(): errors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: wangguixing
+ * @Date: 2023-08-21 09:44:06
+ * @LastEditors: wangguixing
+ * @LastEditTime: 2023-08-21 09:50:09
+ * @FilePath: \src\main.ts
+ */
 import { createApp } from 'vue'
 import stores from './stores/index'
 import App from './App'
